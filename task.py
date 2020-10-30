34eba1a30b161489a7e3b86d36464617f5b02608
Adrian Wysocki <adrian.wyso.97@gmail.com>
2020-10-30 12:48:12 +0100

    second task

--- task.py
+++ task.py
@@ -1,30 +1,44 @@
-# Class diary
-#
-# Create program for handling lesson scores.
-# Use python to handle student (highscool) class scores, and attendance.
-# Make it possible to:
-# - Get students total average score (average across classes)
-# - get students average score in class
-# - hold students name and surname
-# - Count total attendance of student
-#
-# Please, use your imagination and create more functionalities.
-# Your project should be able to handle entire school(s?).
-# If you have enough courage and time, try storing (reading/writing)
-# data in text files (YAML, JSON).
-# If you have even more courage, try implementing user interface (might be text-like).
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#
-#When you are done upload this code to your github repository. 
-#
-#Delete these comments before commit!
-#Good luck.
+#!/usr/bin/env python3
+
+class Student:
+    def __init__(self, name, surname):
+        self._name = name
+        self._surname = surname
+
+    def __str__(self):
+        return '{}_{}'.format(self._name, self._surname)
+
+class Class:
+    def __init__(self, students):
+        self._students = students
+        self._grades = {}
+
+        for student in self._students:
+            key = str(student)
+            self._grades[key] = []
+
+    def get_average_over_class(self):
+        pass
+
+    def get_average_for_student(self, key):
+        pass    
+        
+
+class School:
+    def __init__(self):
+        self.classes = {}
+
+    def add_class(self, class_name, new_class):
+        self.classes[class_name] = new_class
+
+
+if __name__ == '__main__':    
+
+    students = []
+    students.append(Student("Jan", "Kowalski"))
+    students.append(Student("Stachu", "Nowak"))
+
+    new_class = Class(students)
+
+    school = School()
+    school.add_class('1a', new_class)
