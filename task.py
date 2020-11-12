864ccb02aa8779d84741c5fb971abc4714679de0
awysocki <adrian.wyso.97@gmail.com>
2020-11-12 21:27:53 +0100

    final solution

--- task.py
+++ task.py
@@ -1,44 +1,99 @@
 #!/usr/bin/env python3
 
-class Student:
-    def __init__(self, name, surname):
-        self._name = name
-        self._surname = surname
-
-    def __str__(self):
-        return '{}_{}'.format(self._name, self._surname)
-
-class Class:
-    def __init__(self, students):
-        self._students = students
-        self._grades = {}
-
-        for student in self._students:
-            key = str(student)
-            self._grades[key] = []
-
-    def get_average_over_class(self):
-        pass
-
-    def get_average_for_student(self, key):
-        pass    
-        
-
-class School:
-    def __init__(self):
-        self.classes = {}
-
-    def add_class(self, class_name, new_class):
-        self.classes[class_name] = new_class
+import logging
+import json
+import statistics as stat
+import random
 
 
-if __name__ == '__main__':    
+def import_data():
+    with open('schools.json') as f:
+        schools = json.load(f)
+    return schools
 
-    students = []
-    students.append(Student("Jan", "Kowalski"))
-    students.append(Student("Stachu", "Nowak"))
 
-    new_class = Class(students)
+def add_student(school_class, student_name):
+    new_student = {
+        'grades': {key: [] for key in school_class['subjects']},
+        'attendance': []
+    }
+    school_class['students'][student_name] = new_student
 
-    school = School()
-    school.add_class('1a', new_class)
+
+def add_grade(school_class, student_name, sub_name, grade):
+    try:
+        school_class['students'][student_name]['grades'][sub_name].append(
+                grade)
+    except KeyError as err:
+        missed_key = str(err)[1:-1]
+
+        if missed_key == student_name:
+            logging.error('Student {} not found'.format(missed_key))
+
+        elif missed_key == sub_name:
+            logging.error('Subject {} not present for this student'.format(
+                    missed_key))
+
+
+def register_attendace(school_class, student, value):
+    try:
+        school_class['students'][student]['attendance'].append(value)
+    except KeyError:
+        logging.error('Student {} not found'.format(student))
+
+
+def get_student_average(student):
+    grades = student['grades']
+    subject_means = [stat.mean(grade) for grade in grades.values()]
+    return stat.mean(subject_means)
+
+
+def get_students_avg_list(school_class):
+    students = school_class['students']
+    return [get_student_average(student) for student in students.values()]
+
+
+def get_class_average(school_class):
+    return stat.mean(get_students_avg_list(school_class))
+
+
+def get_school_average(school):
+    students_means = []
+    for school_class in school.values():
+        students_means.extend(get_students_avg_list(school_class))
+
+    return stat.mean(students_means)
+
+
+def get_student_attendance_percentage(student):
+    att = student['attendance']
+    print(att)
+    return stat.mean(att) * 100
+
+
+if __name__ == '__main__':
+    schools = import_data()
+
+    first_a = schools['first']['1a']
+
+    sample_student_name = 'SampleName SampleSurname'
+    add_student(first_a, sample_student_name)
+    add_grade(first_a, sample_student_name, 'math', 2.0)
+    add_grade(first_a, sample_student_name, 'math', 3.0)
+    add_grade(first_a, sample_student_name, 'physics', 4.0)
+    add_grade(first_a, sample_student_name, 'history', 5.0)
+
+    add_grade(first_a, sample_student_name, 'now_should_rise', 5.0)
+
+    for _ in range(5):
+        attendance = random.choice([True, False])
+        register_attendace(first_a, sample_student_name, attendance)
+
+    sample_student = first_a['students'][sample_student_name]
+
+    print('Sample student attendance: {}%'.format(
+            get_student_attendance_percentage(sample_student)))
+    print('Sample student avg grade: {}'.format(
+            get_student_average(sample_student)))
+    print('Class avg grade: {}'.format(get_class_average(first_a)))
+    print('School avg grade: {}'.format(get_school_average(schools['first'])))
